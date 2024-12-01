343426466aea988942a062e13f9360b8a8665f51
norikenpi <norikenpi@gmail.com>
2024-12-02 06:56:38 +0900

    feat: derive recipeId from api
    Signed-off-by: norikenpi <norikenpi@gmail.com>

--- proto/api_proto.py
+++ proto/api_proto.py
@@ -55,9 +55,12 @@ def derive_category_list():
 
 def derive_recipe_id(df, recipe_name):
     df_querry = df.query(f'categoryName.str.contains("{recipe_name}")', engine='python')
+    filtered_df = df_querry[df_querry['category3'].notna()]
+    print(filtered_df)
+    best_match_id = filtered_df.iloc[0]['categoryId']
+    print(best_match_id)
     # TODO 小カテゴリまで含まれているもの。一致度が高いもの。
-    recipe_id = "14-121-553"
-    return recipe_id
+    return best_match_id
 
 def derive_ingredients(recipe_id):
     # レシピIDからレシピ情報を取得 https://webservice.rakuten.co.jp/documentation/recipe-detail
@@ -65,6 +68,7 @@ def derive_ingredients(recipe_id):
     json_data = json.loads(res.text)
     ingredient_list = []
     for i in json_data['result']:
+        # TODO 重複を許しているので、重複を除去する
         ingredient_list.extend(i['recipeMaterial'])
     return ingredient_list
 
@@ -72,15 +76,6 @@ def check_allergens_in_recipe(recipe_database, recipe_name, allergens):
     ingredients = recipe_database.get(recipe_name)
     if not ingredients:
         return f"料理名 '{recipe_name}' のレシピが見つかりません。"
-    # 原材料の中にアレルゲンが含まれているかチェック
-    """
-    detected_allergens = []
-    for i in ingredients:
-        for a in allergens:
-            if a in i:
-                detected_allergens.append(a)
-    """
-    
     detected_allergens = [a for a in allergens if any(a in i for i in ingredients)]
 
     return f"この料理にはアレルゲンが含まれています: {','.join(detected_allergens)}"
@@ -90,7 +85,7 @@ if __name__ == '__main__':
     application_id = os.getenv('RAKUTEN_API_APPLICATION_ID')
     #print("derive_category_list")
     #derive_category_list()
-    #print("read_csv")
+    print("read_csv")
     recipe_df = pd.read_csv('data/category_list.csv')
     print("derive_recipe_id")
     cuisine_database = {}
@@ -100,14 +95,25 @@ if __name__ == '__main__':
     print("derive_ingredients")
     ingredient_data = derive_ingredients(cuisine_id)
     #print(ingredient_data)
-    cuisine_database["オムライス"] = ingredient_data
+    cuisine_database[cuisine_name] = ingredient_data
     my_allergens = ["卵", "豆"]
     print(check_allergens_in_recipe(cuisine_database, cuisine_name, my_allergens))
     
 
 
 
+# ベクトル検索を使う場合は、うまいことカテゴリーサイズを使えるかもしれない。
 
+# TODO 卵の量を考慮したチェック（ドーナツ1個分ぐらいはいける）
+# TODO 火が通ってるかどうかのチェック
 
-
-
+# TODO 焼きそばと入力したとき、どの焼きそばか結構難しい
+"""
+      Unnamed: 0  category1  category2  category3   categoryId categoryName
+913          913         16      155.0     1338.0  16-155-1338     あんかけ焼きそば
+914          914         16      155.0      575.0   16-155-575        塩焼きそば
+915          915         16      155.0      574.0   16-155-574      ソース焼きそば
+916          916         16      155.0     2147.0  16-155-2147       かた焼きそば
+918          918         16      155.0      152.0   16-155-152     アレンジ焼きそば
+1243        1243         22      229.0     1017.0  22-229-1017       焼きそばパン
+"""
