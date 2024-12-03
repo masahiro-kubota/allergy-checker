6908049a8169bcbe2a083cdba3ecaa77c38f41bb
Masahiro Kubota <norikenpi@gmail.com>
2024-12-03 22:21:36 +0900

    feat: change function to coroutine
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- proto/openai_api_proto.py
+++ proto/openai_api_proto.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
+import asyncio
 import os
+from time import time
 
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import AsyncOpenAI
 
-def ask_dish_details(dish_name, client):
+async def ask_dish_details(dish_name, client):
     
     response = client.chat.completions.create(
       model="gpt-4o",
@@ -23,6 +24,7 @@ def ask_dish_details(dish_name, client):
     return final_response
 
 def check_ingredient(dish_name, ingredient, dish_details, client):
+    
     response = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
@@ -35,14 +37,45 @@ def check_ingredient(dish_name, ingredient, dish_details, client):
     final_response = response.choices[0].message.content
     return final_response
 
+
+
+async def check_ingredient_async(num, start_check, client):
+    response = await client.chat.completions.create(
+      model="gpt-4o",
+      messages=[
+        {"role": "user", "content": "5文字の単語を出力してください"}],
+      stream=False
+    )
+    final_response = response.choices[0].message.content
+    end = time()
+    elapsed_time = end - start_check
+    print(f"Task {num} finished in {elapsed_time} seconds")
+    print(final_response)
+
+async def async_main(start_time, client):
+    task1 = asyncio.create_task(check_ingredient_async(1, start_time, client))
+    task2 = asyncio.create_task(check_ingredient_async(2, start_time, client))
+    task3 = asyncio.create_task(check_ingredient_async(3, start_time, client))
+    task4 = asyncio.create_task(check_ingredient_async(4, start_time, client))
+    tasks = [task1, task2, task3, task4]
+    await asyncio.gather(*tasks)
+
+
+
 if __name__=="__main__":
+    my_start_time = time()
+    
+    
     load_dotenv()
     application_id = os.getenv('OPENAI_API_KEY')
-    my_dish = input('料理名を入力してください: ')
-    my_client = OpenAI()
+    #my_dish = input('料理名を入力してください: ')
+    my_client = AsyncOpenAI()
+    asyncio.run(async_main(my_start_time, my_client))
+    """
     my_ingredient = "卵"
     my_dish_details = ask_dish_details(my_dish, my_client)
     print(my_dish_details)
     has_egg = check_ingredient(my_dish, my_ingredient, my_dish_details, my_client)
     print(has_egg)
+    """
     
