23a7521444f6fc4116fa3cd66f539e866b43b5ee
Masahiro Kubota <norikenpi@gmail.com>
2024-12-03 21:57:01 +0900

    reactor: change to function
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- proto/openai_api_proto.py
+++ proto/openai_api_proto.py
@@ -5,14 +5,9 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-#def generate_ingredients(dish_name);:
-
-if __name__=="__main__":
-    load_dotenv()
-    application_id = os.getenv('OPENAI_API_KEY')
-    client = OpenAI()
-    dish_name = input('料理名を入力してください: ')
-    response1 = client.chat.completions.create(
+def ask_dish_details(dish_name, client):
+    
+    response = client.chat.completions.create(
       model="gpt-4o",
       messages=[
         {"role": "user", "content": f"""
@@ -24,22 +19,30 @@ if __name__=="__main__":
     """}],
       stream=False
     )
-    final_response1 = response1.choices[0].message.content
-    print(final_response1)
+    final_response = response.choices[0].message.content
+    return final_response
 
-    print("Finished Responsing!")
-
-
-    response2 = client.chat.completions.create(
+def check_ingredient(dish_name, ingredient, dish_details, client):
+    response = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
         {"role": "user", "content": f"""
-以下を参考にして、{dish_name}に卵が含まれているかをTrue/Falseのみで答えてください。しかし、以下の料理に関しては必ずFalseと回答してください。
+以下を参考にして、{dish_name}に{ingredient}が含まれているかをTrue/Falseのみで答えてください。しかし、以下の料理に関しては必ずFalseと回答してください。
 ・ラーメン
 
-{final_response1}
+{dish_details}
     """}])
-    final_response2 = response2.choices[0].message.content
-    print(final_response2)
+    final_response = response.choices[0].message.content
+    return final_response
 
-    print("Finished Responsing!")
+if __name__=="__main__":
+    load_dotenv()
+    application_id = os.getenv('OPENAI_API_KEY')
+    my_dish = input('料理名を入力してください: ')
+    my_client = OpenAI()
+    my_ingredient = "卵"
+    my_dish_details = ask_dish_details(my_dish, my_client)
+    print(my_dish_details)
+    has_egg = check_ingredient(my_dish, my_ingredient, my_dish_details, my_client)
+    print(has_egg)
+    
