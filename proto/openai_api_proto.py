2567971761c060c207068e815d5ce3959a0025af
Masahiro Kubota <norikenpi@gmail.com>
2024-12-03 23:23:15 +0900

    feat: add task
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- proto/openai_api_proto.py
+++ proto/openai_api_proto.py
@@ -9,14 +9,13 @@ from openai import AsyncOpenAI,OpenAI
 def ask_dish_details(dish_name, client, start_time):
     
     response = client.chat.completions.create(
-      model="gpt-4o",
+      model="gpt-4o-mini",
       messages=[
         {"role": "user", "content": f"""
 {dish_name}に関して以下の質問に回答してください。
 
 原材料（10個）:
 火が通っているかどうか：
-1人前あたりに使用されている卵の量：
     """}],
       stream=False
     )
@@ -29,7 +28,7 @@ def ask_dish_details(dish_name, client, start_time):
 def check_ingredient(dish_name, ingredient, dish_details, client):
     
     response = client.chat.completions.create(
-      model="gpt-3.5-turbo",
+      model="gpt-4o-mini",
       messages=[
         {"role": "user", "content": f"""
 以下を参考にして、{dish_name}に{ingredient}が含まれているかをTrue/Falseのみで答えてください。しかし、以下の料理に関しては必ずFalseと回答してください。
@@ -44,49 +43,77 @@ def check_ingredient(dish_name, ingredient, dish_details, client):
 
 async def check_ingredient_async(dish_name, ingredient, dish_details, client, num, start_time):
     response = await client.chat.completions.create(
-      model="gpt-3.5-turbo",
+      model="gpt-4o-mini",
       messages=[
         {"role": "user", "content": f"""
-以下を参考にして、{dish_name}に{ingredient}が含まれているかをTrue/Falseのみで答えてください。しかし、以下の料理に関しては必ずFalseと回答してください。
-・ラーメン
+以下を参考にして、{dish_name}に{ingredient}が含まれているかをTrue/Falseのみで答えてください。
 
 {dish_details}
     """}])
     final_response = response.choices[0].message.content
     end = time()
     elapsed_time = end - start_time
-    print(f"Task {num} finished in {elapsed_time} seconds")
-    print(final_response)
+    print(f"Task {num} finished in {elapsed_time} seconds: {final_response}")
     return final_response
 
-async def async_main(start_time):
+async def check_cooked_async(dish_name, dish_details, client, num, start_time):
+    response = await client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=[
+        {"role": "user", "content": f"""
+以下を参考にして、{dish_name}は火が通っているかをTrue/Falseのみで答えてください。
+
+{dish_details}
+    """}])
+    final_response = response.choices[0].message.content
+    end = time()
+    elapsed_time = end - start_time
+    print(f"Task {num} finished in {elapsed_time} seconds: {final_response}")
+    return final_response
+
+async def check_white_list_async(dish_name, white_list, client, num, start_time):
+    response = await client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=[
+        {"role": "user", "content": f"""
+{dish_name}は以下の料理に含まれるかをTrue/Falseのみで答えてください。
+
+{white_list}
+    """}])
+    final_response = response.choices[0].message.content
+    end = time()
+    elapsed_time = end - start_time
+    print(f"Task {num} finished in {elapsed_time} seconds: {final_response}")
+    return final_response
+
+async def async_main():
     async_client = AsyncOpenAI()
     client = OpenAI()
     my_dish = input('料理名を入力してください: ')
+    start_time = time()
     my_dish_details = ask_dish_details(my_dish, client, start_time)
     print(my_dish_details)
     task1 = asyncio.create_task(check_ingredient_async(my_dish, "卵", my_dish_details, async_client, 1, start_time))
-    task2 = asyncio.create_task(check_ingredient_async(my_dish, "豚肉", my_dish_details, async_client, 2, start_time))
-    task3 = asyncio.create_task(check_ingredient_async(my_dish, "牛乳", my_dish_details, async_client, 3, start_time))
+    task2 = asyncio.create_task(check_cooked_async(my_dish, my_dish_details, async_client, 2, start_time))
+    task3 = asyncio.create_task(check_white_list_async(my_dish, "ラーメン", async_client, 3, start_time))
+    # TODO 回答している時間が長いから、回答が短くなるように質問を分けたほうがいい。
+    # TODO 原材料確認と火が通っているかの確認は別で動かした方が速い。
+    # TODO 追加確認材料 いも類　ナッツ類　甲殻類　貝類　ごぼう　れんこん　こんにゃく　そば　
+    # 追加ホワイトリスト　卵（ラーメン）　イモ類（さつまいも）　果物（柑橘類　いちご　ぶどう　パイナップル　りんご　缶詰）豆（醤油　味噌）　甲殻類（えびせん　桜えび）　　
     tasks = [task1, task2, task3]
     await asyncio.gather(*tasks)
 
-#TODO Trueが含まれているかFalseが含まれているかで後処理する。Trueの方が多かったらTrueにしてしまう。Trueだけを出力しなかった場合記録するか。
-"""
-卵が含まれているかをTrue/Falseのみで答えてください。
-- 牛丼：True
 
-"""
 
 if __name__=="__main__":
-    my_start_time = time()
+    
     
     
     load_dotenv()
     #my_dish = input('料理名を入力してください: ')
     my_async_client = AsyncOpenAI()
     my_clinet = OpenAI()
-    asyncio.run(async_main(my_start_time))
+    asyncio.run(async_main())
     """
     my_ingredient = "卵"
     my_dish_details = ask_dish_details(my_dish, my_client)
@@ -95,3 +122,10 @@ if __name__=="__main__":
     print(has_egg)
     """
     
+
+#TODO Trueが含まれているかFalseが含まれているかで後処理する。Trueの方が多かったらTrueにしてしまう。Trueだけを出力しなかった場合記録するか。
+"""
+卵が含まれているかをTrue/Falseのみで答えてください。
+- 牛丼：True
+
+"""
